eb0bd436104b72c062d5db018521bf410966b971
David Fernandes <davidsf@pantherify.com>
2025-06-01 00:41:33 +0100

    Add project files.

---
+++ Libs/WyN/CSVAmmo.cs
@@ -0,0 +1,34 @@
+﻿namespace Libs.WyN
+{
+    public class CSVAmmo
+    {
+        public string? AmmoRound { get; set; }
+        public float? BaseDamage { get; set; }
+        public float? AreaEffectDamage { get; set; }
+        public double? AreaEffectRadius { get; set; }
+        public float? DetonationDamage { get; set; }
+        public float? DetonationRadius { get; set; }
+        public double? HealthHitModifier { get; set; }
+        public float? Health { get; set; }
+        public float? Mass { get; set; }
+        public float? MaxTrajectory { get; set; }
+        public float? DesiredSpeed { get; set; }
+        public float? EnergyCost { get; set; }
+        public float? GravityMultiplier { get; set; }
+        public float? ShieldModifier { get; set; }
+        public float? ShieldBypass { get; set; }
+        public bool? DisableClientPredictedAmmo { get; set; }
+        public float? FallOffDistance { get; set; }
+        public float? FallOffMinMultipler { get; set; }
+        public float? ByBlockHitMaxAbsorb { get; set; }
+        public float? EndOfLifeMaxAbsorb { get; set; }
+        public float? BackKickForce { get; set; }
+
+        public bool? EnergyBaseDamage { get; set; }
+        public bool? EnergyAreaEffectDamage { get; set; }
+        public bool? EnergyDetonationDamage { get; set; }
+        public bool? EnergyShieldDamage { get; set; }
+
+    }
+
+}
---
+++ Libs/WyN/CSVWeapon.cs
@@ -0,0 +1,17 @@
+﻿namespace Libs.WyN
+{
+    public class CSVWeapon
+    {
+        public string? PartName { get; set; }
+        public float? MaxTargetDistance { get; set; }
+        public float? MinTargetDistance { get; set; }
+        public float? RateOfFire { get; set; }
+        public float? ReloadTime { get; set; }
+        public float? DeviateShotAngle { get; set; }
+        public double? AimingTolerance { get; set; }
+        public float? IdlePower { get; set; }
+        public float? HeatSinkRate { get; set; }
+        public float? HeatPerShot { get; set; }
+    }
+
+}
