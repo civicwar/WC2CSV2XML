6f3fee410c2853f794a8b9a7b01e7296b16e225d
David Fernandes <davidsf@pantherify.com>
2025-06-01 12:29:05 +0100

    organization

---
+++ CSV2XML/ExportHelpers.cs
@@ -0,0 +1,19 @@
+﻿using Libs.WyN;
+
+namespace WC2CSV
+{
+    static class ExportHelpers
+    {
+
+        public static void CSVAmmoToXML(CSVAmmo parts)
+        {
+
+        }
+
+        public static void CSVWeaponToXML(CSVWeapon parts)
+        {
+
+        }
+
+    }
+}
--- CSV2XML/Program.cs
+++ CSV2XML/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WC2CSV
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var parts = new WyN.WeaponCore.Parts();
+
+            ExportHelpers.CSVToXML(parts);
+            ExportHelpers.CSVToXML(parts);
+
+        }
+    }
+
+}
